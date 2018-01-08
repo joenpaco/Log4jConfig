63d3a78c7d63c77ea1b3e96969cc375c386fba6c
Jose Parico <joenpaco@gmail.com>
2018-01-08 08:58:50 -0430

    Initial

---
+++ src/test/java/models/Log4J2PropertiesConfTest.java
@@ -0,0 +1,12 @@
+package models;
+
+import org.junit.Test;
+
+public class Log4J2PropertiesConfTest {
+
+	@Test
+	public void testPerformSomeTask() throws Exception {
+		Log4J2PropertiesConf log4J2PropertiesConf = new Log4J2PropertiesConf();
+		log4J2PropertiesConf.performSomeTask();
+	}
+}
